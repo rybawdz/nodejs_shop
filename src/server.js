75e16df9e193f19e054644918172a21f10586d7a
rybawdz <miriambouh@gmail.com>
2023-12-16 12:30:08 +0100

    Finish prometheus and grafana setup and add basic dashboard

--- src/server.js
+++ src/server.js
@@ -1,17 +1,36 @@
+const express = require('express');
+const http = require('http');
 const logger = require('./logger');
-const client = require('prom-client')
 const colors = require('colors')
-startTime = Date.now()
+const url = require('url')
+const promBundle = require("express-prom-bundle");
+var app = express();
+
+const metricsMiddleware = promBundle({
+  includeMethod: true, 
+  includePath: true, 
+  includeStatusCode: true, 
+  includeUp: true,
+  customLabels: {project_name: 'hello_world', project_type: 'test_metrics_labels'},
+  promClient: {
+      collectDefaultMetrics: {
+      }
+    }
+});
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(metricsMiddleware);
+
+
+app.get('/', (req, res) => {res.send('Hello World');})
+
+
 
 function start(){
    logger.info(colors.cyan(`Server running on port ${colors.bold(`4040`)}`));
-
-   var http = require("http");
-   http.createServer(function (request, response) {
-      response.writeHead(200, {'Content-Type': 'text/plain'});
-      response.end('Hello World\n');
-   }).listen(4040);
-
+   http.createServer(app).listen(4040);
 }
 
 module.exports = 
