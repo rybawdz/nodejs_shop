ae3147e9ef63aae1747070e9e98bb420386fcdc0
rybawdz <miriambouh@gmail.com>
2023-12-24 21:15:40 +0100

    fix mongodb init script

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,8 +1,26 @@
-use ecommerce
 
-db.createCollectidon('users')
+print('Start #################################################################');
+var adminDB = db.getSiblingDB('admin');
+var authResult = db.auth('root', 'example');
+
+var db = db.getSiblingDB('ecommerce');
+
+db.createUser({
+    user: 'euser',
+    pwd: 'changesecret',
+    roles: [
+      { role: 'readWrite', db: 'ecommerce' }
+    ]
+  });
+db.createCollection('users')
 db.createCollection('products')
 db.createCollection('invoices')
 db.createCollection('reviews')
 db.createCollection('receipts')
+db.createCollection('orders')
+
+
+
+
+
 
