e071b7e3c13459fc973657271eafbf896a51bfc5
JarekKadecki <95314594+JarekKadecki@users.noreply.github.com>
2024-01-22 08:32:56 +0100

    introducing store item tile with styling

--- src/shop-frontend/src/app/page.jsx
+++ src/shop-frontend/src/app/page.jsx
@@ -2,18 +2,20 @@
 import Button from './components/button';
 import { useRouter } from 'next/navigation';
 import NavBar from './components/navbar';
-import './styles/main.css'
+import StoreItem from './components/storeItem';
+import './styles/main.css';
 
 function Home() {
   const router = useRouter();
   return (
     <div>
-    <NavBar></NavBar>
+    <NavBar/>
     <div className='content'>
     <marquee>
     <h1>Hello, home page!</h1>
     </marquee>
-
+    {/* add photo request handling to routing */}
+    <StoreItem photoPath='./img/photo.jpg' itemName="itemName" itemPrice="100"/>
     </div>
     {/* <Button text="log in" action={() => router.push("/login")}/> 
     <Button text="sign up" action={() => router.push("/signup")}/>  */}
