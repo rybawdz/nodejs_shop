32a2435bf530f8645b9db314be215b751f502b4c
rybawdz <miriambouh@gmail.com>
2024-02-01 13:43:34 +0100

    fix product search api

--- src/server/routes/productsearch.js
+++ src/server/routes/productsearch.js
@@ -3,8 +3,8 @@ const Product = require("../models/product");
 module.exports = async function productSearch(req, res) {
 
     try {
-        const query = req.query.query;
-        if (!query) {
+        const searchText = req.query.name;
+        if (!searchText) {
             return res.status(400).json({ error: 'Missing search query' });
         }
         const data = await Product.find({
@@ -13,9 +13,12 @@ module.exports = async function productSearch(req, res) {
               { description: { $regex: searchText, $options: 'i' } }, 
             ],
           });
+          console.log(data);
+          console.log(searchText);
         if(data != null){
             res.status(200).json(data);
         }
+        
         else{
             res.status(201).json({message: 'No products found.'});
         }
