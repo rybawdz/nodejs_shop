7b454bf7506be8c2516073fbf75acb9d509dca8a
Jarek Kadecki <332771@uwr.edu.pl>
2024-02-05 10:37:03 +0000

    styling admin panel

--- src/shop-frontend/src/app/adminpanel/page.jsx
+++ src/shop-frontend/src/app/adminpanel/page.jsx
@@ -3,11 +3,20 @@ import { useState, useEffect } from 'react';
 import ProductPanel from '../components/productPanel'
 import processForm from '../lib/processForm';
 import getUserData from '../lib/getUserData';
+import NavBar from '../components/navbar';
+
+import '../styles/main.css';
+import '../styles/profileStyles.css';
 
 export default function Page() {
   const [userOrders, setUserOrders] = useState(null);
   const [userData, setUserData] = useState(null);
   const [userBaskets, setUserBaskets] = useState(null);
+  const [ordersNavbarStyles, setOrdersNavbarStyles] = useState({ backgroundColor: '#B0C400' });
+  const [basketsNavbarStyles, setBasketsNavbarStyles] = useState({ backgroundColor: '#B0C400' });
+  const [productNavbarStyles, setProductNavbarStyles] = useState({ backgroundColor: '#B0C400' });
+  const [usersNavbarStyles, setUsersNavbarStyles] = useState({ backgroundColor: '#B0C400' });
+  const [dataInfoContent, setDataInfoContent] = useState(null);
 
   useEffect(() => {
     getUserData('http://localhost:4040/api/v1/admin/orders', setUserOrders);
@@ -18,11 +27,13 @@ export default function Page() {
   var orders = null;
   if(userOrders){
     orders = userOrders.map( (order) => (
-        <div key={order._id}>
-          <p>UserID: {order.user._id}</p>
-          <p>Email: {order.user.email}</p>
-          <p>Date: {order.date}</p>
-          <p>Address: {order.address}</p>
+        <div key={order._id} className='orderAdminTile'>
+          <div className='orderTileTitle'>
+            <p><label className='metaText'>UserID: </label>{order.user._id}</p>
+            <p><label className='metaText'>Email: </label>{order.user.email}</p>
+            <p><label className='metaText'>Date: </label>{order.date}</p>
+            <p><label className='metaText'>Address: </label>{order.address}</p>
+          </div>
           {order.items.map( (item) => (
             <div key={  item._id}>
               <p>Product: {item.product.name}</p>
@@ -37,11 +48,11 @@ export default function Page() {
   var users = null;
   if(userData){
     users = userData.map( (user) => (
-        <div key={user._id}>
-          <p>UserID: {user._id}</p>
-          <p>Email: {user.email}</p>
-          <p>Role: {user.role}</p>
-          <p>Profile Image: {user.profileImage}</p>
+        <div key={user._id} className='userAdminTile'>
+          <p><label className='metaText'>UserID: </label>{user._id}</p>
+          <p><label className='metaText'>Email: </label>{user.email}</p>
+          <p><label className='metaText'>Role: </label>{user.role}</p>
+          <p><label className='metaText'>Profile Image: </label>{user.profileImage}</p>
         </div>
         ));
   }
@@ -49,32 +60,73 @@ export default function Page() {
   var baskets = null;
   if(userBaskets){
     baskets = userBaskets.map( (basket) => (
-        <div key={basket._id}>
-          <p>UserID: {basket.user._id}</p>
-          <p>Email: {basket.user.email}</p>
+        <div key={basket._id} className='basketAdminTile'>
+          <div className='userTitleTile'>
+            <p><label className='metaText'>UserID: </label>{basket.user._id}</p>
+            <p><label className='metaText'>Email: </label>{basket.user.email}</p>
+          </div>
           {basket.items.map( (item) => (
             <div key={  item._id}>
-              <p>Product: {item.product.name}</p>
-              <p>Quantity: {item.quantity}</p>
-              <p>Price: {item.product.price}</p>
+              <p><label className='metaText'>Product: </label>{item.product.name}</p>
+              <p><label className='metaText'>Quantity: </label>{item.quantity}</p>
+              <p><label className='metaText'>Price: </label>{item.product.price}</p>
             </div>
           ))}
         </div>
         ));
   }
+      
+  var product = <ProductPanel submit={processForm} />;
+  
+  // wybacz, że kod z takimi powtórzeniami 
+  // ale obecnie mam energię mentalną na wykończeniu
+
+  function displayData(data) {
+    setBasketsNavbarStyles({ backgroundColor: '#B0C400' });
+    setOrdersNavbarStyles({ backgroundColor: '#B0C400' });
+    setUsersNavbarStyles({ backgroundColor: '#B0C400' });
+    setProductNavbarStyles({ backgroundColor: '#B0C400' });
+    if(data == 'orders') {
+      setDataInfoContent(orders);
+      setOrdersNavbarStyles({ backgroundColor: '#D5DE2E' });
+    }
+    else if(data == 'baskets') {
+      setDataInfoContent(baskets);
+      setBasketsNavbarStyles({ backgroundColor: '#D5DE2E' });
+    }
+    else if(data == 'users') {
+      setDataInfoContent(users);
+      setUsersNavbarStyles({ backgroundColor: '#D5DE2E' });
+    }
+    else if(data == 'product') {
+      setDataInfoContent(product);
+      setProductNavbarStyles({ backgroundColor: '#D5DE2E' });
+    }
+
+  }
 
   return (
   <>
-    <ProductPanel submit={processForm} />
-    <h1>Orders</h1>
-    {orders && 
-      orders}
-    <h1>Users</h1>
-    {users &&
-      users}
-    <h1>Baskets</h1>
-    {baskets &&
-      baskets}
+    <NavBar/>
+    <div className='content'>
+      <div className='dataInfoNavbar'>
+              <div className='ordersTile' style={productNavbarStyles} onClick={() => displayData('product')}>
+                <p>Product</p>
+              </div>
+              <div className='ordersTile' style={usersNavbarStyles} onClick={() => displayData('users')}>
+                <p>Users</p>
+              </div>
+              <div className='ordersTile' style={ordersNavbarStyles} onClick={() => displayData('orders')}>
+                <p>Orders</p>
+              </div>
+              <div className='basketTile' style={basketsNavbarStyles} onClick={() => displayData('baskets')}>
+                <p>Baskets</p>
+              </div>
+      </div>
+      <div className='dataInfo'>
+        {dataInfoContent}
+      </div>
+    </div>
   </>
   );
 }
