3d08e6be3614e5d328ec50f616f75b4e1876a6b9
Jarek Kadecki <332771@uwr.edu.pl>
2024-02-06 03:19:56 +0000

    adding contact page

--- src/shop-frontend/src/app/contact/page.jsx
+++ src/shop-frontend/src/app/contact/page.jsx
@@ -1,3 +1,28 @@
+import NavBar from "../components/navbar";
+import "../styles/main.css";
+
 export default function Page() {
-    return <h1>Hello, contact page!</h1>
+    return( 
+    <>
+      <NavBar/>
+      <div className="content">
+        <section>
+            <h1>Contact Us - Zesty Shrek Factory</h1>
+            <h2>Get in Ogre Touch</h2>
+            
+            <p>Welcome to our Swamp! If you have any inquiries, suggestions, or just want to share your love for Shrek, feel free to reach out to us:</p>
+
+            <p><strong>Email:</strong> info@shrekmerch.com</p>
+
+            <p><strong>Phone:</strong> 1-800-OGRE-LUV</p>
+
+            <p>Donkey says: "We're always ready to chat, especially if it involves onions and ogre-sized adventures!"</p>
+        </section>
+
+        <footer>
+            Disclaimer: All communications will be handled with the utmost Ogre professionalism. No Farquaad-level shenanigans, we promise! 🌟
+        </footer>
+      </div>
+    </>
+    )
   }
